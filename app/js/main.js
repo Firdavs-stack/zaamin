a75b27f7c422a3708f09e2a755dbba814923115c
Front-stack <firdavsusmanov418@gmail.com>
2023-11-22 01:09:45 +0500

    mobile view added & some gsap animations added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
+const restourantsSlider = new Swiper(".restourants__slider",{
+  loop: true,
+})
+
 const headerSlider = new Swiper(".header__slider", {
     loop: true,
     fade: true,
@@ -20,11 +24,18 @@ questItems.forEach(item => item.addEventListener('click',(e) => {
   console.log(e.target.parentNode);
   if(item == e.target && item == e.target.parentNode){
       item.classList.remove('__active');
-      alert('jopa');
     }else{
   item.classList.toggle('__active');}
 }));
 
+
+const burger = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.header__mobile-menu');
+
+burger.addEventListener('click',() => {
+  burger.classList.toggle('__active');
+  burgerMenu.classList.toggle('__active');
+})
 
 async function initMap() {
   await ymaps3.ready;
@@ -45,3 +56,64 @@ async function initMap() {
 }
 
 initMap();
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const t1 = gsap.timeline({
+  scrollTrigger: 'body',
+})
+
+t1.from('.header',{
+  opacity: 0,
+},0,1)
+.from('.navbar',{
+  opacity:0
+},0,1)
+.from('.logo',{
+  x:-250,
+  opacity:0
+},0.2)
+.from('.header__menu-item',{
+  stagger: 0.1,
+  x:250,
+  opacity:0
+},0.2)
+.from('.header__title',{
+  y: 250,
+  opacity:0
+},0,3)
+.from('.header__subtitle',{
+  y: 250,
+  opacity:0
+},0.4)
+.from('.header__workingTime',{
+  y: -250,
+  opacity:0
+},0.5)
+.from('.pricing',{
+  y: 250,
+  opacity:0
+},0.6)
+
+
+const t2 = gsap.timeline({
+  scrollTrigger: '.what-do',
+})
+
+t2.from('.what-do .block__title',{
+  y:-250,
+  opacity:0
+},0.1)
+.from('.what-do .block__item:first-child',{
+  x: -250,
+  opacity:0
+},0,2)
+.from('.what-do .block__item:last-child',{
+  x: 250,
+  opacity:0
+},0.3)
+.from('.what-do .block__item:not(:last-child):not(:first-child)',{
+  y: -250,
+  opacity:0
+},0.5)
